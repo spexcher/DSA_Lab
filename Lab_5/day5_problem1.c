9cdef0adad876113888dd1c1bbabf6bf2a75c712
Gourab Modak <137685981+spexcher@users.noreply.github.com>
2024-08-28 14:58:50 +0530

    Update day5_problem1.c

--- Lab_5/day5_problem1.c
+++ Lab_5/day5_problem1.c
@@ -1,15 +1,11 @@
-/*----------------------------------------------------------------
-AUTHOR: spexcher
-Name: Gourab Modak
-email: gm2623@cse.jgec.ac.in
-Institution: Jalpaiguri Government Engineering College (B. Tech UG CSE 4 year)
-Github: https://github.com/gauravthedeveloper/
-linkedin : https://www.linkedin.com/in/gourabmodak/
-codeforces : https://codeforces.com/profile/sectumsempra07/
-codechef : https://www.codechef.com/users/sectumsempra07/
-leetcode : https://leetcode.com/sectumsempra07/
---------------------------------------------------------------------
-*/
+// ------------------------------------------------------------------------------------
+// Author: spexcher
+// Name: Gourab Modak
+// email: spexcher@gmail.com
+// Linktree: https://linktr.ee/spexcher/
+// Github: https://github.com/spexcher/
+// linkedin : https://www.linkedin.com/in/gourabmodak/
+// ------------------------------------------------------------------------------------
 #include <stdio.h>
 
 #define MAX_SIZE (int)1e7
